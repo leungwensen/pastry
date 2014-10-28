a206960e6a5fad07a2b386c2f8ddbcecfe01e581
绝云 <wensen.lws@alibaba-inc.com>
2014-10-28 18:50:42 +0800

    generating dag graph for dependency tree of pastry

---
+++ bin/js/dependencyTree.js
@@ -0,0 +1,126 @@
+#!/usr/bin/env node
+// !!!!! WARNING ONLY works on OS X with Chrome.app installed !!!!!
+/* jshint strict: true, undef: true, unused: true */
+// /* global */
+
+'use strict';
+/*
+ * @author      : 绝云
+ * @description : 分析项目的依赖情况，并图形化显示
+ * @syntax      : 在 bin 文件夹所在的父文件夹中 ./bin/dependencyTree.js
+ */
+
+// 数据准备 {
+    var
+    // graph 相关变量 {
+        nodes      = [],
+        links      = [],
+        nodeLoaded = {},
+    // }
+    // 文件服务器相关 {
+        fileServer,
+        port = 3000,
+        path = 'demo/depTree',
+    // }
+    // 系统依赖 {
+        exec        = require('child_process').exec,
+        fs          = require('fs'),
+        madge       = require('madge'),
+        pastry      = require('pastry'),
+        staticSever = require('node-static'),
+    // }
+    // 获取分析结果 {
+        madgeObj = madge('src/', {
+            // exclude           : /core/,
+            format            : 'amd',
+            // mainRequireModule : 'src/pastry',
+            // requireConfig     : ''
+        }),
+
+        depTree  = madgeObj.tree,
+        circular = madgeObj.circular(),
+        ids      = pastry.keys(depTree);
+    // }
+// }
+// 私有函数 {
+    function classById (id) {
+        switch (true) {
+            case /^core/.test(id):
+                return 'type-core';
+            case /^amd/.test(id):
+                return 'type-core';
+            case /shim\//.test(id):
+                return 'type-common';
+            case /fmt\//.test(id):
+                return 'type-common';
+            default:
+                return 'type-default';
+        }
+    }
+    function addNodeById (id) {
+        if (!nodeLoaded[id]) {
+            nodes.push({
+                id        : id,
+                label     : id,
+                nodeclass : classById(id)
+            });
+            nodeLoaded[id] = true;
+        }
+    }
+// }
+// 生成 graph 数据 {
+    ids = pastry.keys(depTree);
+    pastry.each(ids, function(id) {
+        addNodeById(id);
+    });
+    pastry.each(depTree, function(deps, id) {
+        pastry.each(deps, function(dep) {
+            dep = dep.replace(/\.\.\//g, '');
+            addNodeById(dep);
+            links.push({
+                id    : 'S' + dep + 'T' + id,
+                source: dep,
+                target: id
+            });
+        });
+    });
+// }
+// 是否有循环依赖 {
+    console.log(circular.getArray(), !!circular.getArray().length);
+// }
+// 写入 graph 数据 {
+    fs.writeFile(path + '/json/nodes.json', JSON.stringify(nodes, null, '\t'), 'utf-8', function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    fs.writeFile(path + '/json/links.json', JSON.stringify(links, null, '\t'), 'utf-8', function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    fs.writeFile(path + '/json/depTree.json', JSON.stringify(depTree, null, '\t'), 'utf-8', function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+// }
+// 启动一个文件服务器 {
+    fileServer = new staticSever.Server('./');
+
+    require('http').createServer(function (request, response) {
+        request.addListener('end', function () {
+            fileServer.serve(request, response);
+        }).resume();
+    }).listen(port);
+// }
+// 打开浏览器 {
+    exec('open -a "/Applications/Google Chrome.app" \'http://127.0.0.1:3000/' + path + '/index.html\'',
+        function (error) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        }
+    );
+// }
+
---
+++ demo/depTree/js/index.js
@@ -0,0 +1,139 @@
+
+/* jshint strict: true, undef: true, unused: true */
+/* global d3, dagreD3, $, pastry */
+
+(function() {
+    'use strict';
+    /*
+     * @author      : 绝云(wensen.lws@alibaba-inc.com)
+     * @description : 画图
+     */
+
+    var
+        // 缩放相关 {
+            zoom,
+        // }
+        // 画图相关 {
+            g,
+            svg = d3.select('svg'),
+            graphData = {
+                nodes: [],
+                links: []
+            },
+            layout = dagreD3.layout()
+                .nodeSep(50)
+                .rankSep(200)
+                .rankDir('TB');
+        // }
+
+    function svgTranslate (graph, option) {
+        /*
+         * @description : 图形变换
+         * @parameter   : {Number} x, x
+         * @parameter   : {Number} y, y
+         * @parameter   : {Number} scale, 缩放倍数
+         * @parameter   : {Number} duration, 动画持续时间
+         */
+        var x        = option.x,
+            y        = option.y,
+            scale    = option.scale || 1,
+            duration = pastry.isNumber(option.duration) ?
+                option.duration : 300;
+
+        if (pastry.isNumber(x) && pastry.isNumber(y)) {
+            graph
+                .transition()
+                .duration(duration)
+                .attr(
+                    'transform',
+                    'translate(' + x + ', ' + y + ')scale(' + scale + ')'
+                );
+            // 图在自动定位后要更新 d3.event 里缓存了的 scale 和 translate，否则会有跳动的问题 {
+                zoom.translate([x, y]);
+                zoom.scale(scale);
+            // }
+        }
+    }
+
+    function draw (graph) {
+        /*
+         * @description: 画图
+         */
+        g = new dagreD3.Digraph();
+        // 加 node {
+            pastry.each(graph.nodes, function(node) {
+                if (node.id) {
+                    g.addNode(node.id,  node);
+                }
+            });
+        // }
+        // 加 link {
+            pastry.each(graph.links, function(link) {
+                g.addEdge(link.id, link.source, link.target);
+            });
+        // }
+        // 画图 {
+            var renderer     = new dagreD3.Renderer(),
+                oldDrawNodes = renderer.drawNodes();
+        // }
+        // 定制 node {
+            renderer.drawNodes(function(graph, root) {
+                var svgNodes = oldDrawNodes(graph, root);
+                svgNodes.each(function(u) {
+                    d3.select(this).classed(graph.node(u).nodeclass, true);
+                });
+                return svgNodes;
+            });
+        // }
+        // zooming {
+            var graphLayout = renderer
+                    .layout(layout)
+                    .run(g, d3.select('svg')),
+                zoomed = function () {
+                    svg.select('g')
+                        .attr(
+                            'transform',
+                            'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')'
+                        );
+                };
+            zoom = d3.behavior.zoom()
+                .scaleExtent([0.2, 3])
+                .on('zoom', zoomed);
+            svg.call(zoom);
+        // }
+        // 居中 {
+            svgTranslate(svg.select('g'), {
+                x: (($('#canvas').width()  - graphLayout.graph().width)  / 2),
+                y: (($('#canvas').height() - graphLayout.graph().height) / 2),
+            });
+        // }
+    }
+    function initNamespaces () {
+        var id,
+            result = [];
+        pastry.each(graphData.nodes, function(node) {
+            id = node.id;
+            if (pastry.isString(id)) {
+                result.push(id.split('/')[0]);
+            }
+        });
+    }
+
+    $.get('./json/nodes.json', function(nodes) {
+        $.get('./json/links.json', function(links) {
+            // 备份图数据 {
+                graphData = {
+                    nodes: nodes,
+                    links: links
+                };
+            // }
+            // 生成 namespaces 菜单 {
+                initNamespaces();
+            // }
+            // 画图 {
+                draw(graphData);
+            // }
+        });
+    });
+}());
+
