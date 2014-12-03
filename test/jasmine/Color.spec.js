9ec63044af28f0655fdfdb0d9286efa5adc6d1e8
leungwensen <leungwensen@gmail.com>
2014-12-03 18:24:22 +0800

    tests for Color & color/named

---
+++ test/jasmine/Color.spec.js
@@ -0,0 +1,33 @@
+/* jshint strict: true, undef: true, unused: true */
+/* global describe, it, expect */
+
+require([
+    'pastry',
+    'Color'
+], function (
+    pastry,
+    Color
+) {
+    'use strict';
+
+    describe('Color', function () {
+        it('Color initProps', function () {
+            var color = new Color();
+            expect(color.r).toBe(255);
+            expect(color.g).toBe(255);
+            expect(color.b).toBe(255);
+            expect(color.a).toBe(1);
+        });
+        it('white is #fff', function () {
+            expect(Color.fromString('white').toRgba()).toEqual((new Color('#fff')).toRgba());
+        });
+        it('white is also #ffffff', function () {
+            expect(Color.fromString('white').toRgba()).toEqual((new Color('#ffffff')).toRgba());
+        });
+        it('maroon is #800000', function () {
+            expect(Color.fromString('maroon').toRgba()).toEqual((new Color('#800000')).toRgba());
+        });
+
+    });
+});
+
