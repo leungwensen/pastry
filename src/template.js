006baf39f72bf2656c6281a04e7959ae6bb17153
绝云 <wensen.lws@alibaba-inc.com>
2014-12-12 15:53:37 +0800

    a simple template engine finished

--- src/template.js
+++ src/template.js
@@ -1,12 +1,12 @@
-/* jshint strict: true, undef: true, unused: true */
+/* jshint strict: true, undef: true */
 /* global define */
 
 define('template', [
-    'pastry'
-    // 'dom/construct'
+    'pastry',
+    'html/utils'
 ], function(
-    pastry
-    // domConstruct
+    pastry,
+    htmlUtils
 ) {
     'use strict';
     /*
@@ -14,46 +14,68 @@ define('template', [
      * @description : template engine
      */
     var template,
-        cache = {};
+        cache     = {},
+        helper    = {},
+        RE_parser = /([\s'\\])(?!(?:[^{]|\{(?!%))*%\})|(?:\{%(=|#)([\s\S]+?)%\})|(\{%)|(%\})/g;
         // defaultOpitons = {}; // TODO add grammar aliases, etc.
 
+    function render (s, p1, p2, p3, p4, p5) {
+        if (p1) { // whitespace, quote and backspace in HTML context
+            return {
+                "\n": "\\n",
+                "\r": "\\r",
+                "\t": "\\t",
+                " " : " "
+            }[p1] || "\\" + p1;
+        }
+        if (p2) { // interpolation: {%=prop%}, or unescaped: {%#prop%}
+            if (p2 === "=") {
+                return "'+_e(typeof" + p3 + "==='undefined'?'':" + p3 + ")+'";
+            }
+            return "'+(" + p3 + "==null?'':" + p3 + ")+'";
+        }
+        if (p4) { // evaluation start tag: {%
+            return "';";
+        }
+        if (p5) { // evaluation end tag: %}
+            return "_s+='";
+        }
+    }
+
+    // add helpers to pastry to pass to compiled functions, can be extended {
+        // helper.escape = htmlUtils.escape;
+        pastry.extend(helper, htmlUtils);
+    // }
+
     return pastry.template = template = {
+        helper: helper,
         compile: function (str/*, option*/) {
             // option = pastry.extend({}, defaultOpitons, option);
             if (!pastry.isString(str)) {
                 return str;
             }
-            /*jshint -W054*/
-            return cache[str] || (cache[str] = new Function('obj',
-                    'var p = [],' +
-                        'print = function(){' +
-                            'p.push.apply(p, arguments);' +
-                        '};' +
-                    // Introduce the data as local variables using with(){}
-                    'with(obj){' +
-                        'p.push("' +
-                            // Convert the template into pure JavaScript
-                            // (function () { // tobe extended
-                                str
-                                    .replace(/[\r\t\n]/g, ' ')
-                                    .split("<%").join('\t')
-                                    .replace(/((^|%>)[^\t]*)'/g, '$1\r')
-                                    .replace(/\t=(.*?)%>/g, "',$1,'")
-                                    .split('\t').join("');")
-                                    .split('%>').join("p.push('")
-                                    .split('\r').join("\\'") +
-                                    // .split('\r').join("\\'");
-                                // return str;
-                            // }()) +
-                        '");' +
-                    '};' +
-                    'return p.join("");'
+
+            /*jshint -W054*/ // new Function()
+            return cache[str] || (cache[str] = new Function('obj', 'helper',
+                    "var _e=helper.escape," +
+                        "print=function(s,e){" +
+                            "_s+=e?(s==null?'':s):_e(s);" +
+                        "};" +
+                    "with(obj){" +
+                        // include helper {
+                            // "include = function (s, d) {" +
+                            //     "_s += tmpl(s, d);}" + "," +
+                        // }
+                        "_s='" + str.replace(RE_parser, render) + "';" +
+                    "}" +
+                    "return _s;"
                 )
             );
         },
         render: function (str, data/*, option*/) {
             // option = option || {};
-            return template.compile(str/*, option*/)(data);
+            // console.log(template.compile(str).toString());
+            return template.compile(str/*, option*/)(data, template.helper);
         }
     };
 });
